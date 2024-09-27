7253d6275262ad589435e7b661f15df7a5dd2a4f
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-27 16:20:15 +0700

    story-5

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val units = 250
-    var totalElecBill = when {
-        units > 200 -> 250 + (units-200) * 2
-        units in 101..200 -> 100 + (units-100) * 1.5
-        units in 0..100 -> units
-        else -> "Units mustn't negative"
-    }
-    println("Final Price: $$totalElecBill")
+    val secretNum = 7
+    val guessed = arrayOf(5,3,7,0)
+    var i = 0
+    do{
+        println(guessed[i])
+        if (secretNum.equals(guessed[i])) break
+        i++
+    } while (true)
 }
