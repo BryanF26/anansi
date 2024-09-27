1d774e17c07ec3984229a381d8f41d26232e68fd
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-27 15:23:05 +0700

    story-3

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val years = 3
-    val salary = 8000000
-    var bonus = when {
-        years > 5 -> salary * 0.2
-        years in 2..5 -> salary * 0.1
-        years in 0..2 -> 0
-        else -> "Years mustn't negative"
+    val spends = 200
+    var finalPrice = when {
+        spends > 200 -> spends * 0.7
+        spends > 100 && spends <= 200 -> spends * 0.8
+        spends > 50 && spends <= 100 -> spends * 0.9
+        spends in 0..50 -> spends
+        else -> "Spends mustn't negative"
     }
-    println("Bonus: " + bonus)
+    println("Final Price: $finalPrice")
 }
