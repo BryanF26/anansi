c781d691841ff50be0473ce30f1dfc70db6edc84
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-27 14:49:21 +0700

    story-1 fix

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    var scores = 80
-    var grades = if(scores in 90..100) "A" else if (scores >= 80) "B" else if (scores >= 70) "C" else if (scores >= 60) "D" else if(scores in 0..59) "F" else "Masukkan nilai range 0 sampai 100"
+    var scores = 90
+    var grades = if(scores in 90..100) "A" else if (scores in 80..89) "B" else if (scores in 70..79) "C" else if (scores in 60..69) "D" else if(scores in 0..59) "F" else "Masukkan nilai range 0 sampai 100"
     println("Grades: " + grades)
 }
