4a5f856c176d3e20629c389a786beaf2741e693e
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-27 14:30:55 +0700

    Update Main.kt

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
     println("Hello World!")
+
+    var num = 12;
+    println(num.and(17))
+
+    val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
+    cars.forEach { println("$it") }
 }
