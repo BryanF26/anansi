768e06e00b76a4a77ec363419664f0f581731bd3
Bryan Felix <bryanfelix2001@gmail.com>
2024-10-01 10:18:21 +0700

    pre test day 3

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -10,6 +10,13 @@
 //    }
 //}
 
+import kotlinx.coroutines.*
+
+fun myCoroutine() {
+    delay(1000L)
+    println("Coroutine executed")
+}
+
 fun reserveSeat(name: String, seatNumber: Int, availableSeats:Array<Int>) {
     if (availableSeats.contains(seatNumber)) {
         availableSeats[seatNumber-1] = 0  // Mark the seat as reserved by setting it to 0
@@ -23,4 +30,5 @@ fun main() {
     val availableSeats = arrayOf(1, 2, 3, 4, 5)
     reserveSeat("John", 2, availableSeats)
     reserveSeat("Sarah", 2, availableSeats)
+    myCoroutine()
 }
